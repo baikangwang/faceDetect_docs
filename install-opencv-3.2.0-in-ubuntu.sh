cc08f35143cc20080c8b8612e3e8064301832b0d
baikangwang <baikangwang@hotmail.com>
2017-08-24 14:24:58 +0800

    update 3.X script

--- install-opencv-3.2.0-in-ubuntu.sh
+++ install-opencv-3.2.0-in-ubuntu.sh
@@ -1,24 +1,40 @@
-sudo apt-get install --assume-yes build-essential cmake git
-sudo apt-get install --assume-yes pkg-config unzip ffmpeg qtbase5-dev python-dev python3-dev python-numpy python3-numpy
-sudo apt-get install --assume-yes libopencv-dev libgtk-3-dev libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libtiff5-dev libjasper-dev
-sudo apt-get install --assume-yes libavcodec-dev libavformat-dev libswscale-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
-sudo apt-get install --assume-yes libv4l-dev libtbb-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev
-sudo apt-get install --assume-yes libvorbis-dev libxvidcore-dev v4l-utils python-vtk
-sudo apt-get install --assume-yes liblapacke-dev libopenblas-dev checkinstall
-sudo apt-get install --assume-yes libgdal-dev
-mkdir opencv
-cd opencv
-wget https://github.com/opencv/opencv/archive/3.2.0.zip -O opencv-3.2.0.zip
-unzip opencv-3.2.0.zip
-cd opencv-3.2.0
-mkdir build
-cd build/    
-cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=/usr/local -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_V4L=ON -DWITH_QT=ON -DWITH_OPENGL=ON -DWITH_CUBLAS=ON -DCUDA_NVCC_FLAGS="-D_FORCE_INLINES" -DWITH_GDAL=ON -DWITH_XINE=ON -DBUILD_EXAMPLES=ON ..
-make -j $(($(nproc) + 1))
+sudo apt install -y --no-install-recommends \
+    libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev \
+    libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libgtk2.0-dev \
+    liblapacke-dev checkinstall
+
+#sudo git clone https://github.com/opencv/opencv.git /usr/local/src/opencv
+#sudo git clone https://github.com/opencv/opencv_contrib.git /usr/local/src/opencv_contrib
+wget https://github.com/opencv/opencv/archive/3.3.0.tar.gz -O opencv-3.3.0.tar.gz && \
+tar -xvf opencv-3.3.0.tar.gz && \
+sudo mv opencv-3.3.0 /usr/local/src/opencv
+
+wget https://github.com/opencv/opencv_contrib/archive/3.3.0.tar.gz -O opencv_contrib-3.3.0.tar.gz && \
+tar -xvf opencv_contrib-3.3.0.tar.gz && \
+sudo mv opencv_contrib-3.3.0 /usr/local/src/opencv_contrib
+
+cd /usr/local/src/opencv
+sudo mkdir build
+cd build
+sudo cmake -D CMAKE_INSTALL_PREFIX=/usr/local \
+        -D BUILD_TESTS=OFF \
+        -D BUILD_opencv_gpu=OFF \
+        -D BUILD_PERF_TESTS=OFF \
+        -D WITH_IPP=OFF \
+        -D OPENCV_EXTRA_MODULES_PATH=/usr/local/src/opencv_contrib/modules \
+        -D OPENCV_EXTRA_MODULES_PATH=/usr/local/src/opencv_contrib/modules -D BUILD_opencv_xfeatures2d=OFF /usr/local/src/opencv \
+        -D OPENCV_EXTRA_MODULES_PATH=/usr/local/src/opencv_contrib/modules -D BUILD_opencv_dnn_modern=OFF /usr/local/src/opencv \
+        -D OPENCV_EXTRA_MODULES_PATH=/usr/local/src/opencv_contrib/modules -D BUILD_opencv_dnns_easily_fooled=OFF /usr/local/src/opencv \
+        -D PYTHON_DEFAULT_EXECUTABLE=$(which python3) ..
+sudo make -j $(($(nproc) + 1))
 sudo make install
+
 sudo /bin/bash -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
-sudo apt-get update
+sudo apt update
+
+cd /usr/local/src/opencv
+sudo rm -r build
 
 # Reboot your machine and run following command to check installed version
 # pkg-config --modversion opencv
